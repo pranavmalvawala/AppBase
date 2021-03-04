2d1e284f1fe916c03997560a501ae0a8d9e61057
Jeremy Zongker <jeremy@trilitech.com>
2021-03-04 04:21:19 +0000

    Added messaging api.

--- interfaces/AccessManagement.ts
+++ interfaces/AccessManagement.ts
@@ -17,7 +17,7 @@ export interface SwitchAppResponseInterface { appName: string, churchId: string 
 export interface UserInterface { id?: string, email?: string, authGuid?: string, displayName?: string, registrationDate?: Date, lastLogin?: Date, password?: string }
 
 export interface ApiConfig { keyName: string, url: string, jwt: string, permisssions: RolePermissionInterface[] }
-export type ApiListType = "AccessApi" | "MembershipApi" | "AttendanceApi" | "GivingApi" | "StreamingLiveApi" | "B1Api";
+export type ApiListType = "AccessApi" | "MembershipApi" | "AttendanceApi" | "GivingApi" | "MessagingApi" | "StreamingLiveApi" | "B1Api";
 export interface IPermission {
     api: string;
     contentType: string;
