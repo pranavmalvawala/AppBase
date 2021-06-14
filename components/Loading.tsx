55d114eae8c2665b3ff5f4ddcb29155af54230e6
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-06-14 22:18:00 +0530

    refactor: perform linting

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,27 +3,23 @@ import { Dots } from "react-activity";
 
 interface Props { size?: "sm" | "md" | "lg", loadingText?: string }
 
-
 export const Loading: React.FC<Props> = (props) => {
 
-    const getContents = () => {
-        const text = (props.loadingText) ? props.loadingText : "Loading"
-        switch (props.size) {
-            case "sm":
-                return <><Dots speed={0.8} animating={true} size={20} /><p style={{ fontSize: 12 }}>{text}</p></>
-                break;
-            case "lg":
-                return <><Dots speed={0.8} animating={true} size={48} /><p style={{ fontSize: 30 }}>{text}</p></>
-                break;
-            default:
-                return <><Dots speed={0.8} animating={true} size={32} /><p>{text}</p></>
-                break;
-        }
+  const getContents = () => {
+    const text = (props.loadingText) ? props.loadingText : "Loading"
+    switch (props.size) {
+    case "sm":
+      return <><Dots speed={0.8} animating={true} size={20} /><p style={{ fontSize: 12 }}>{text}</p></>
+    case "lg":
+      return <><Dots speed={0.8} animating={true} size={48} /><p style={{ fontSize: 30 }}>{text}</p></>
+    default:
+      return <><Dots speed={0.8} animating={true} size={32} /><p>{text}</p></>
     }
+  }
 
-    return (
-        <div style={{ textAlign: "center", fontFamily: "Roboto" }}>
-            {getContents()}
-        </div>
-    )
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Roboto" }}>
+      {getContents()}
+    </div>
+  )
 }
