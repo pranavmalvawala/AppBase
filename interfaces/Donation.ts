e47c77b2ddf9fa18c9d92b751d1a711105cc6b54
Jared Martin <jared@dsprinting.com>
2021-07-21 14:41:14 -0500

    Add amount to Fund interface (#72)
    Co-authored-by: Jared Martin <jared@livechurchsolutions.org>

--- interfaces/Donation.ts
+++ interfaces/Donation.ts
@@ -4,7 +4,7 @@ export interface DonationBatchInterface { id?: string, name?: string, batchDate?
 export interface DonationInterface { id?: string, batchId?: string, personId?: string, donationDate?: Date, amount?: number, method?: string, methodDetails?: string, notes?: string, person?: PersonInterface, fund?: FundInterface }
 export interface DonationSummaryInterface { week: number, donations?: DonationSummaryDonation[] }
 export interface DonationSummaryDonation { totalAmount: number, fund?: FundInterface }
-export interface FundInterface { id: string, name: string }
+export interface FundInterface { id: string, name: string, amount?: number }
 export interface FundDonationInterface { id?: string, donationId?: string, fundId?: string, amount?: number, donation?: DonationInterface }
 export interface PaymentMethodInterface { id?: string, churchId?: string, personId?: string, customerId?: string, email?: string, name?: string }
 export interface StripeCardUpdateInterface { paymentMethodId: string, cardData: StripeCardDataInterface }
