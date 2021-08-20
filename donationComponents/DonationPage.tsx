398b5f74de682144cbd61862edd1bdcc31256a6c
Jared Martin <jared@dsprinting.com>
2021-08-20 15:08:48 -0500

    Move donations from Chums to AppBase (#87)
    Co-authored-by: Jared Martin <jared@livechurchsolutions.org>

---
+++ donationComponents/DonationPage.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+import { DonationForm, RecurringDonations, PaymentMethods } from "./components";
+import { DisplayBox, Loading } from "../components";
+import { ApiHelper, DateHelper, UniqueIdHelper, CurrencyHelper } from "../helpers";
+import { DonationInterface, PersonInterface, StripePaymentMethod } from "../interfaces";
+import { Link } from "react-router-dom"
+import { Table } from "react-bootstrap";
+
+interface Props { personId: string }
+
+export const DonationPage: React.FC<Props> = (props) => {
+  const [donations, setDonations] = React.useState<DonationInterface[]>(null);
+  const [stripePromise, setStripe] = React.useState<Promise<Stripe>>(null);
+  const [paymentMethods, setPaymentMethods] = React.useState<StripePaymentMethod[]>(null);
+  const [customerId, setCustomerId] = React.useState(null);
+  const [person, setPerson] = React.useState<PersonInterface>(null);
+
+  const loadData = () => {
+    if (!UniqueIdHelper.isMissing(props.personId)) {
+      ApiHelper.get("/donations?personId=" + props.personId, "GivingApi").then(data => setDonations(data));
+      ApiHelper.get("/gateways", "GivingApi").then(data => {
+        if (data.length && data[0]?.publicKey) {
+          setStripe(loadStripe(data[0].publicKey));
+          ApiHelper.get("/paymentmethods/personid/" + props.personId, "GivingApi").then(results => {
+            if (!results.length) setPaymentMethods([]);
+            else {
+              let cards = results[0].cards.data.map((card: any) => new StripePaymentMethod(card));
+              let banks = results[0].banks.data.map((bank: any) => new StripePaymentMethod(bank));
+              let methods = cards.concat(banks);
+              setCustomerId(results[0].customer.id);
+              setPaymentMethods(methods);
+            }
+          });
+          ApiHelper.get("/people/" + props.personId, "MembershipApi").then(data => { setPerson(data) });
+        }
+        else setPaymentMethods([]);
+      });
+    }
+  }
+
+  const handleDataUpdate = () => {
+    setPaymentMethods(null);
+    loadData();
+  }
+
+  const getRows = () => {
+    let rows: JSX.Element[] = [];
+
+    if (donations.length === 0) {
+      rows.push(<tr key="0"><td>Donations will appear once a donation has been entered.</td></tr>);
+      return rows;
+    }
+
+    for (let i = 0; i < donations.length; i++) {
+      let d = donations[i];
+      rows.push(
+        <tr key={i}>
+          <td><Link to={"/donations/" + d.batchId}>{d.batchId}</Link></td>
+          <td>{DateHelper.formatHtml5Date(d.donationDate)}</td>
+          <td>{d.method}</td>
+          <td>{d.fund.name}</td>
+          <td>{CurrencyHelper.formatCurrency(d.fund.amount)}</td>
+        </tr>
+      );
+    }
+    return rows;
+  }
+
+  const getTableHeader = () => {
+    const rows: JSX.Element[] = []
+
+    if (donations.length > 0) {
+      rows.push(<tr key="header"><th>Batch</th><th>Date</th><th>Method</th><th>Fund</th><th>Amount</th></tr>);
+    }
+
+    return rows;
+  }
+
+  React.useEffect(loadData, []);
+
+  const getTable = () => {
+    if (!donations) return <Loading />;
+    else return (<Table>
+      <thead>{getTableHeader()}</thead>
+      <tbody>{getRows()}</tbody>
+    </Table>);
+  }
+
+  const getPaymentMethodComponents = () => {
+    if (!paymentMethods) return <Loading />;
+    if (paymentMethods.length) return (
+      <>
+        <PaymentMethods person={person} customerId={customerId} paymentMethods={paymentMethods} stripePromise={stripePromise} dataUpdate={handleDataUpdate} />
+        <RecurringDonations customerId={customerId} paymentMethods={paymentMethods} />
+        <DonationForm person={person} customerId={customerId} paymentMethods={paymentMethods} stripePromise={stripePromise} donationSuccess={handleDataUpdate} />
+      </>
+    );
+    return <PaymentMethods person={person} customerId={customerId} paymentMethods={paymentMethods} stripePromise={stripePromise} dataUpdate={handleDataUpdate} />;
+  }
+
+  return (
+    <>
+      {getPaymentMethodComponents()}
+      <DisplayBox headerIcon="fas fa-hand-holding-usd" headerText="Donations">
+        {getTable()}
+      </DisplayBox>
+    </>
+  );
+}
---
+++ donationComponents/components/index.tsx
@@ -0,0 +1,8 @@
+export { BankForm } from "./BankForm";
+export { CardForm } from "./CardForm";
+export { DonationForm } from "./DonationForm";
+export { PaymentMethods } from "./PaymentMethods";
+export { RecurringDonations } from "./RecurringDonations";
+export { RecurringDonationsEdit } from "./RecurringDonationsEdit";
+export { FundDonations } from "./FundDonations";
+export { FundDonation } from "./FundDonation";
