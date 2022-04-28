e17b79eb4c1c10be31025b9cd8ce203792086b4e
Jeremy Zongker <jeremy@trilitech.com>
2022-04-28 09:48:01 -0500

    Added color support to loading component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Dots } from "react-activity";
+import "react-activity/dist/library.css";
 
-interface Props { size?: "sm" | "md" | "lg", loadingText?: string }
+interface Props { size?: "sm" | "md" | "lg", loadingText?: string, color?: string }
 
 export const Loading: React.FC<Props> = (props) => {
 
   const getContents = () => {
     const text = (props.loadingText) ? props.loadingText : "Loading"
-    let result = <><Dots speed={0.8} animating={true} size={32} /><p>{text}</p></>
+    const color = (props.color) ? props.color : "#222"
+    let result = <><Dots speed={0.8} animating={true} size={32} color={color} /><p style={{ color: color }}>{text}</p></>
     switch (props.size) {
       case "sm":
-        result = <><Dots speed={0.8} animating={true} size={20} /><p style={{ fontSize: 12 }}>{text}</p></>
+        result = <><Dots speed={0.8} animating={true} size={20} color={color} /><p style={{ fontSize: 12, color: color }}>{text}</p></>
         break;
       case "lg":
-        result = <><Dots speed={0.8} animating={true} size={48} /><p style={{ fontSize: 30 }}>{text}</p></>
+        result = <><Dots speed={0.8} animating={true} size={48} color={color} /><p style={{ fontSize: 30, color: color }}>{text}</p></>
         break;
     }
     return result;
