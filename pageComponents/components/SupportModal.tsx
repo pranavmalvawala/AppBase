f829e5830e2ea850bf79d52907424acd5c297483
Jeremy Zongker <jeremy@trilitech.com>
2022-06-03 08:57:10 -0500

    Updated support url

--- pageComponents/components/SupportModal.tsx
+++ pageComponents/components/SupportModal.tsx
@@ -14,14 +14,14 @@ export const SupportModal: React.FC<Props> = (props) => {
   }
 
   const getSubject = () => {
-    if(props.appName)
+    if (props.appName)
       return `?subject=${props.appName} Support`;
     else
       return "";
   }
 
   return (<>
-    <div style={{ position: "fixed", cursor: "pointer", color: "#03a9f4", fontSize: "30px", bottom: "1px", right: "10px"}} onClick={toggleShowSupport}><i className="fas fa-question-circle" /></div>
+    <div style={{ position: "fixed", cursor: "pointer", color: "#03a9f4", fontSize: "30px", bottom: "1px", right: "10px" }} onClick={toggleShowSupport}><i className="fas fa-question-circle" /></div>
     <Modal show={showSupport} onHide={toggleShowSupport} backdrop="static">
       <Modal.Header closeButton>
         <Modal.Title>Get Support</Modal.Title>
@@ -30,11 +30,11 @@ export const SupportModal: React.FC<Props> = (props) => {
         <p><b><i className="fas fa-envelope fa-fw" /> Email:</b> <a href={"mailto:support@livecs.org" + getSubject()}>support@livecs.org</a></p>
         <p><b><i className="fas fa-mobile fa-fw" /> Phone:</b> <a href="tel:+19189942638">+1 (918) 994-2638</a></p>
         <p><b><i className="fab fa-facebook-messenger fa-fw" /> Messenger:</b> <a href="https://m.me/livecsolutions" target="_new">https://m.me/livecsolutions</a></p>
-        <p><b><i className="fas fa-info-circle fa-fw" /> Knowledge Base:</b> <a href="http://support.churchapps.org" target="_new">http://support.churchapps.org</a></p>
+        <p><b><i className="fas fa-info-circle fa-fw" /> Knowledge Base:</b> <a href="https://support.churchapps.org" target="_new">https://support.churchapps.org</a></p>
       </Modal.Body>
       <Modal.Footer style={{ justifyContent: "center" }}>
         <Button onClick={toggleShowSupport}>Close</Button>
       </Modal.Footer>
     </Modal>
-    </>);
+  </>);
 };
