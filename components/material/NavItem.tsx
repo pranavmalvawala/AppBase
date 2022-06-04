44827b66df56ade80f078c51f18c55c42da33056
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-06-04 15:47:39 +0530

    fix NavItem

--- components/material/NavItem.tsx
+++ components/material/NavItem.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { ListItemButton, ListItemIcon, ListItemText, styled } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
-interface Props { url: string, label: string, icon: JSX.Element }
+interface Props {
+  url: string;
+  label: string;
+  icon: JSX.Element;
+}
 
-export const NavItem: React.FC<Props> = props => (
-  <NavLink to={props.url}>
-    <ListItemButton>
-      <ListItemIcon>{props.icon}</ListItemIcon>
-      <ListItemText primary={props.label} />
-    </ListItemButton>
-  </NavLink>
-);
+export const NavItem: React.FC<Props> = (props) => {
+  const StyledNavLink = styled(NavLink)({
+    color: "black",
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "none",
+      color: "black"
+    }
+  });
+
+  return (
+    <StyledNavLink to={props.url}>
+      <ListItemButton sx={{ color: "black" }}>
+        <ListItemIcon>{props.icon}</ListItemIcon>
+        <ListItemText primary={props.label} />
+      </ListItemButton>
+    </StyledNavLink>
+  );
+};
