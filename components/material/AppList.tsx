b18967de0cdb334782826d09b57d55aad059eb31
PhotoPaul <PhotoPaul@gmail.com>
2022-06-15 18:23:19 +0300

    Keep authentication and church when switching apps

--- components/material/AppList.tsx
+++ components/material/AppList.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { ApiHelper } from "../../helpers/ApiHelper";
+import { UserHelper } from "../../helpers/UserHelper";
 import { NavItem } from "./NavItem";
 
-export const AppList: React.FC = () => (
-  <>
-    <NavItem url="https://app.chums.org/login" external={true} label="Chums" icon="logout" />
-    <NavItem url="https://streaminglive.church/login" external={true} label="StreamingLive" icon="logout" />
-    <NavItem url="https://b1.church/login" external={true} label="B1.Church" icon="logout" />
-    <NavItem url="https://lessons.church/login" external={true} label="Lessons.church" icon="logout" />
-  </>
-);
+export const AppList: React.FC = () => {
+  const jwt = ApiHelper.getConfig("AccessApi").jwt;
+  const churchId = UserHelper.currentChurch.id;
+  return (
+    <>
+      <NavItem url={`https://app.chums.org/login?jwt=${jwt}&churchId=${churchId}`} external={true} label="Chums" icon="logout" />
+      <NavItem url={`https://streaminglive.church/login?jwt=${jwt}&churchId=${churchId}`} external={true} label="StreamingLive" icon="logout" />
+      <NavItem url={`https://b1.church/login?jwt=${jwt}&churchId=${churchId}`} external={true} label="B1.Church" icon="logout" />
+      <NavItem url={`https://lessons.church/login?jwt=${jwt}&churchId=${churchId}`} external={true} label="Lessons.church" icon="logout" />
+    </>
+  );
+}
