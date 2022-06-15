2fbf7cd08c0b8b111f1f95936183c3f76b156fae
Jeremy Zongker <jeremy@trilitech.com>
2022-06-15 12:28:25 -0500

    Context needs to be passed into appbase instead of fetched from consuming project

--- components/material/ChurchList.tsx
+++ components/material/ChurchList.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import UserContext from "../../../UserContext";
+import { UserContextInterface } from "../../interfaces";
 import { UserHelper } from "../../helpers/UserHelper";
 import { ChurchInterface } from "../../interfaces";
 import { NavItem } from "./NavItem";
 
-export interface Props { churches: ChurchInterface[], currentChurch: ChurchInterface }
+export interface Props { churches: ChurchInterface[], currentChurch: ChurchInterface, context: UserContextInterface, }
 
 export const ChurchList: React.FC<Props> = props => {
   const location = useLocation();
-  const context = React.useContext(UserContext);
 
   if (props.churches.length < 2) return <></>;
   else {
@@ -18,7 +17,7 @@ export const ChurchList: React.FC<Props> = props => {
     churches.forEach(c => {
       const church = c;
       const churchName = c.name;
-      result.push(<NavItem url={location.pathname} selected={(c.id === props.currentChurch.id) && true} onClick={() => UserHelper.selectChurch(context, church.id, null)} label={churchName} icon="church" />);
+      result.push(<NavItem url={location.pathname} selected={(c.id === props.currentChurch.id) && true} onClick={() => UserHelper.selectChurch(props.context, church.id, null)} label={churchName} icon="church" />);
     });
     return <>{result}</>;
   }
