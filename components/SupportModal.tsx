9af584ae00bf2df7a3b23233f02fa5309507ecf2
Taras <taras.sydor@outlook.com>
2022-07-06 21:28:27 +0300

    Fixed style SupportModal

--- components/SupportModal.tsx
+++ components/SupportModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon, Stack } from "@mui/material";
 import React from "react";
 
 interface Props {
@@ -17,10 +17,10 @@ export const SupportModal: React.FC<Props> = (props) => {
     <Dialog open={true} {...props}>
       <DialogTitle>Get Support</DialogTitle>
       <DialogContent>
-        <p><b><Icon>mail</Icon> Email:</b> <a href={"mailto:support@livecs.org" + getSubject()}>support@livecs.org</a></p>
-        <p><b><Icon>phone_iphone</Icon> Phone:</b> <a href="tel:+19189942638">+1 (918) 994-2638</a></p>
-        <p><b><Icon>forum</Icon> Messenger:</b> <a href="https://m.me/livecsolutions" target="_new">https://m.me/livecsolutions</a></p>
-        <p><b><Icon>info</Icon> Knowledge Base:</b> <a href="https://support.churchapps.org" target="_new">https://support.churchapps.org</a></p>
+        <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>mail</Icon> Email:</Stack></b> <a href={"mailto:support@livecs.org" + getSubject()}>support@livecs.org</a></Stack>
+        <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>phone_iphone</Icon> Phone:</Stack></b> <a href="tel:+19189942638">+1 (918) 994-2638</a></Stack>
+        <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>forum</Icon> Messenger:</Stack></b> <a href="https://m.me/livecsolutions" target="_new">https://m.me/livecsolutions</a></Stack>
+        <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>info</Icon> Knowledge Base:</Stack></b> <a href="https://support.churchapps.org" target="_new">https://support.churchapps.org</a></Stack>
       </DialogContent>
       <DialogActions>
         <Button variant="outlined" onClick={props.onClose}>Close</Button>
