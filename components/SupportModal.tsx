a08322ba27fcd4a2ebfdc087b9d92d55e2ae3ab7
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2022-07-15 08:04:40 +0530

    fix: resolve support modal warnings (#155)

--- components/SupportModal.tsx
+++ components/SupportModal.tsx
@@ -6,24 +6,20 @@ interface Props {
   onClose: () => void
 }
 
-export const SupportModal: React.FC<Props> = (props) => {
-
-  const getSubject = () => {
-    if (props.appName) return `?subject=${props.appName} Support`;
-    else return "";
-  }
+export const SupportModal: React.FC<Props> = ({ appName = "", onClose }) => {
+  const subject = appName ? `?subject=${appName} Support` : ""
 
   return (<>
-    <Dialog open={true} {...props}>
+    <Dialog open={true} onClose={onClose}>
       <DialogTitle>Get Support</DialogTitle>
       <DialogContent>
-        <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>mail</Icon> Email:</Stack></b> <a href={"mailto:support@livecs.org" + getSubject()}>support@livecs.org</a></Stack>
+        <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>mail</Icon> Email:</Stack></b> <a href={"mailto:support@livecs.org" + subject}>support@livecs.org</a></Stack>
         <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>phone_iphone</Icon> Phone:</Stack></b> <a href="tel:+19189942638">+1 (918) 994-2638</a></Stack>
         <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}} mb={2}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>forum</Icon> Messenger:</Stack></b> <a href="https://m.me/livecsolutions" target="_new">https://m.me/livecsolutions</a></Stack>
         <Stack direction="row" alignItems="center" sx={{flexWrap: "wrap"}}><b><Stack direction="row" alignItems="center" mr="5px"><Icon sx={{marginRight: "5px"}}>info</Icon> Knowledge Base:</Stack></b> <a href="https://support.churchapps.org" target="_new">https://support.churchapps.org</a></Stack>
       </DialogContent>
       <DialogActions>
-        <Button variant="outlined" onClick={props.onClose}>Close</Button>
+        <Button variant="outlined" onClick={onClose}>Close</Button>
       </DialogActions>
     </Dialog>
   </>);
