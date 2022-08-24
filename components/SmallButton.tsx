a7eaccdf37e8cb2c3250beae415d99036ad33b07
Jeremy Zongker <jeremy@trilitech.com>
2022-08-24 15:45:19 -0500

    Fixed SmallButton href links

--- components/SmallButton.tsx
+++ components/SmallButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Icon, Tooltip } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   ariaLabel?: string;
@@ -12,7 +13,13 @@ interface Props {
 }
 
 export const SmallButton = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const handleClick = (e: React.MouseEvent) => { e.preventDefault(); props.onClick(e); }
+  const navigate = useNavigate()
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (props.href) navigate(props.href);
+    else props.onClick(e);
+  }
 
   const style = (props.text)
     ? { backgroundColor: props.color, "& span": { marginRight: 1 } }
@@ -20,7 +27,7 @@ export const SmallButton = React.forwardRef<HTMLDivElement, Props>((props, ref) 
 
   return (
     <Tooltip title={props.toolTip || ""} arrow placement="top">
-      <Button sx={style} variant={props.text ? "outlined" : "text"} color={props.color} aria-label={props.ariaLabel || "editButton"} onClick={(props.onClick) ? handleClick : null} size="small" href={props.href}>
+      <Button sx={style} variant={props.text ? "outlined" : "text"} color={props.color} aria-label={props.ariaLabel || "editButton"} onClick={handleClick} size="small">
         <Icon>{props.icon}</Icon>{(props.text) ? props.text : ""}
       </Button>
     </Tooltip>
--- interfaces/Doing.ts
+++ interfaces/Doing.ts
@@ -1,3 +1,7 @@
+export interface ActionInterface { id?: string, automationId?: string, actionType?: string, actionData?: string }
+export interface AutomationInterface { id?: string, title: string, active: boolean }
+export interface Condition { id?: string, conditionGroupId?: string, field?: Date, fieldData?: Date, operator?: string, value?: string }
+export interface ConditionGroup { id?: string, automationId?: string, parentGroupId?: string, groupType?: string }
 export interface TaskInterface {
   id?: string,
   taskNumber?: number,
