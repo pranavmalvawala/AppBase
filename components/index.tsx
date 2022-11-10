497230eaceb9f35f1f63eec189479470669a0ae3
Jeremy Zongker <jeremy@trilitech.com>
2022-11-10 10:35:01 -0600

    Moved markdown editor to appBase

--- components/index.tsx
+++ components/index.tsx
@@ -1,14 +1,16 @@
+export { CreatePerson } from "./CreatePerson";
 export { ErrorMessages } from "./ErrorMessages";
 export { ExportLink } from "./ExportLink";
 export { DisplayBox } from "./DisplayBox";
-export { HelpIcon } from "./HelpIcon";
-export { InputBox } from "./InputBox";
-export { PersonAdd } from "./PersonAdd";
-export { Loading } from "./Loading";
-export { QuestionEdit } from "./QuestionEdit";
-export { FormSubmissionEdit } from "./FormSubmissionEdit";
-export { SmallButton } from "./SmallButton";
 export { FloatingSupport } from "./FloatingSupport";
+export { FormSubmissionEdit } from "./FormSubmissionEdit";
+export { HelpIcon } from "./HelpIcon";
 export { ImageEditor } from "./ImageEditor";
-export { CreatePerson } from "./CreatePerson";
+export { InputBox } from "./InputBox";
+export { Loading } from "./Loading";
+export { MarkdownEditor } from "./MarkdownEditor";
+export { PersonAdd } from "./PersonAdd";
+export { QuestionEdit } from "./QuestionEdit";
+export { SmallButton } from "./SmallButton";
+
 export * from "./material";
