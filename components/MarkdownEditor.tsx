8ef2dcd58b55e0597f28b43c8d13f5ebee3e6a14
Jeremy Zongker <jeremy@trilitech.com>
2022-12-12 15:48:28 -0600

    Temp rename markdown to handle case sensitivity issue

--- components/MarkdownEditor.tsx
+++ components/MarkdownEditor.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-import { Markdown } from "./Markdown";
+import { Markdown } from "./Markdown2";
 
 type Props = {
   value: string;
