94c8f246db344b39ec1d1d63bc6bfb4ccfcdce70
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2022-12-15 19:02:56 +0530

    remove code and comment actions (#180)

--- components/markdownEditor/MarkdownEditor.tsx
+++ components/markdownEditor/MarkdownEditor.tsx
@@ -1,5 +1,17 @@
-import React from "react"
 import { Box } from "@mui/material";
+import {
+  bold,
+  italic,
+  strikethrough,
+  hr,
+  title,
+  divider,
+  link,
+  quote,
+  unorderedListCommand,
+  orderedListCommand,
+  checkedListCommand,
+} from "@uiw/react-md-editor/lib/commands";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 
@@ -12,7 +24,25 @@ interface Props {
 export function MarkdownEditor({ value, onChange, editor: MDEditor }: Props) {
   return (
     <Box sx={{ border: "1px solid currentColor", borderRadius: 2 }}>
-      <MDEditor value={value} onChange={onChange} preview="edit" />
+      <MDEditor
+        value={value}
+        onChange={onChange}
+        commands={[
+          bold,
+          italic,
+          strikethrough,
+          hr,
+          title,
+          divider,
+          link,
+          quote,
+          divider,
+          unorderedListCommand,
+          orderedListCommand,
+          checkedListCommand,
+        ]}
+        preview="edit"
+      />
     </Box>
   );
 }
