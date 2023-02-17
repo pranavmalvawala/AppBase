529c2efd60b52791b8de074872baf20597c446e4
Jeremy Zongker <jeremy@zongker.net>
2023-02-17 11:48:02 -0600

    Made appearance optional

--- components/material/SiteWrapper.tsx
+++ components/material/SiteWrapper.tsx
@@ -11,7 +11,7 @@ interface Props {
   children: React.ReactNode,
   appName: string,
   router?: any,
-  appearance: AppearanceInterface
+  appearance?: AppearanceInterface
 }
 
 const OpenDrawer = styled(Drawer)(
