24066ca845db262ed53ed2add96bbec07e80c822
Jeremy Zongker <jeremy@zongker.net>
2023-04-04 19:54:05 -0500

    Added visibility

--- interfaces/Content.ts
+++ interfaces/Content.ts
@@ -2,6 +2,6 @@ export interface LinkInterface { id?: string; churchId?: string; url?: string; t
 export interface SermonInterface { id?: string, churchId?: string, playlistId?: string, videoType?: string, videoData?: string, videoUrl?: string, title?: string, description?: string, publishDate?: Date, thumbnail?: string, duration?: number, permanentUrl?: boolean }
 export interface PlaylistInterface { id?: string, churchId?: string, title?: string, description?: string, publishDate?: Date, thumbnail?: string }
 export interface StreamingServiceInterface { id?: string, churchId?: string, serviceTime?: Date, earlyStart?: number, duration: number, chatBefore: number, chatAfter: number, provider: string, providerKey: string, videoUrl: string, timezoneOffset: number, recurring: boolean, label: string, sermonId?: string }
-export interface EventInterface { id?: string; churchId?: string; groupId?: string; start?: Date; end?:Date, title?: string; description?: string; allDay?: boolean; }
+export interface EventInterface { id?: string; churchId?: string; groupId?: string; start?: Date; end?:Date, title?: string; description?: string; allDay?: boolean; visibility?:string; }
 export interface RecurringEventInterface { id?: string; churchId?: string; groupId?: string; start?: Date, rule?: string; }
 export interface RecurringExceptionInterface { id?: string; churchId?: string; recurringEventId?: string; eventId?: string; originalStart?: Date; }
